e3af4f550e2fb6be23c122efd18c6dc80661d89b
GarandTyson <gttyson@stanford.edu>
2019-07-14 15:32:06 -0700

    Added build scripts

--- app/.externalNativeBuild/cmake/debug/x86/cmake_install.cmake
+++ app/.externalNativeBuild/cmake/debug/x86/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/WaterProject/WaterInsightsV005/app
+# Install script for directory: D:/WaterInsights/WaterInsights-Android/app
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/WaterProject/WaterInsightsV005/app/.externalNativeBuild/cmake/debug/x86/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/WaterInsights/WaterInsights-Android/app/.externalNativeBuild/cmake/debug/x86/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
---
+++ buildScripts/BuildCV.bat
@@ -0,0 +1,4 @@
+start xcopy ..\OpenCV-src\x86_64\libopencv_java4.so ..\app\src\main\jniLibs\x86_64\libopencv_java4.so* /y
+start xcopy ..\OpenCV-src\x86\libopencv_java4.so ..\app\src\main\jniLibs\x86\libopencv_java4.so* /y
+start xcopy ..\OpenCV-src\arm64-v8a\libopencv_java4.so ..\app\src\main\jniLibs\arm64-v8a\libopencv_java4.so* /y
+start xcopy ..\OpenCV-src\armeabi-v7a\libopencv_java4.so ..\app\src\main\jniLibs\armeabi-v7a\libopencv_java4.so* /y
