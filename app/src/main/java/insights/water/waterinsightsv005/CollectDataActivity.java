3c7237ee24379ce9dfe87ae257fa40fcf23f9bb4
GarandTyson <gttyson@stanford.edu>
2019-07-14 16:23:40 -0700

    Cleaned up cmake

--- app/src/main/java/insights/water/waterinsightsv005/CollectDataActivity.java
+++ app/src/main/java/insights/water/waterinsightsv005/CollectDataActivity.java
@@ -18,7 +18,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
