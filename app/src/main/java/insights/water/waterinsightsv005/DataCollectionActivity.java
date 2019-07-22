a5b5e8d31c69255b654ece318f6e535fdb18ed8c
GarandTyson <gttyson@stanford.edu>
2019-07-21 22:11:59 -0700

    Added timer

--- app/src/main/java/insights/water/waterinsightsv005/DataCollectionActivity.java
+++ app/src/main/java/insights/water/waterinsightsv005/DataCollectionActivity.java
@@ -1,5 +1,7 @@
 package insights.water.waterinsightsv005;
 
+import android.os.Build;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -8,15 +10,43 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+import android.widget.TextView;
 
 public class DataCollectionActivity extends AppCompatActivity {
+
+    TextView timerText;
+    public int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_collection);
 
+        timerText = findViewById(R.id.timer_view);
+        timerText.setText("0:30");
         triggerStep1Popup();
+    }
+
+    private void setTimer(int seconds) {
+        counter = seconds;
+        new CountDownTimer(seconds * 1000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                String time;
+                time = String.valueOf(counter / 60);
+                time += ":";
+                if (counter % 60 < 10) {
+                    time += "0";
+                }
+                time += String.valueOf(counter % 60);
+                timerText.setText(time);
+                counter--;
+            }
+            @Override
+            public void onFinish() {
+                timerText.setText(getString(R.string.finished_string));
+            }
+        }.start();
     }
 
     private void triggerStep1Popup() {
@@ -30,6 +60,10 @@ public class DataCollectionActivity extends AppCompatActivity {
         int height = LinearLayout.LayoutParams.WRAP_CONTENT;
         boolean focusable = true; // lets taps outside the popup also dismiss it
         final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            popupWindow.setElevation(20);
+        }
 
         /* Launch popup once activity begins running */
         findViewById(android.R.id.content).post(new Runnable() {
