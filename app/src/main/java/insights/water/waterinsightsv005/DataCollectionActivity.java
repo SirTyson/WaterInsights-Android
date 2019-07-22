dcc32d270ea6d8431e4e996f5b9799326fd47030
GarandTyson <gttyson@stanford.edu>
2019-07-21 21:08:43 -0700

    Popup skeleton

--- app/src/main/java/insights/water/waterinsightsv005/DataCollectionActivity.java
+++ app/src/main/java/insights/water/waterinsightsv005/DataCollectionActivity.java
@@ -2,6 +2,12 @@ package insights.water.waterinsightsv005;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 public class DataCollectionActivity extends AppCompatActivity {
 
@@ -9,5 +15,36 @@ public class DataCollectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_collection);
+
+        triggerStep1Popup();
+    }
+
+    private void triggerStep1Popup() {
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup_collection_step1, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        /* Launch popup once activity begins running */
+        findViewById(android.R.id.content).post(new Runnable() {
+            public void run() {
+                popupWindow.showAtLocation(findViewById(android.R.id.content), Gravity.CENTER, 0, 0);
+            }
+        });
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
     }
 }
