895b9ee9d8c19b5540df33487f6249d5ed00b750
GarandTyson <gttyson@stanford.edu>
2019-07-21 18:04:56 -0700

    Finished initial info UI

--- app/src/main/java/insights/water/waterinsightsv005/InitialInformationActivity.java
+++ app/src/main/java/insights/water/waterinsightsv005/InitialInformationActivity.java
@@ -1,11 +1,14 @@
 package insights.water.waterinsightsv005;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,6 +16,8 @@ import java.util.List;
 public class InitialInformationActivity extends AppCompatActivity {
 
     private Spinner waterTypeSpinner;
+    private AppCompatButton continueButton;
+
     private int spinnerSize;
     private int spinnerSelectedIndex;
 
@@ -22,8 +27,26 @@ public class InitialInformationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_initial_information);
 
         waterTypeSpinner = findViewById(R.id.water_type_spinner);
+        continueButton = findViewById(R.id.continue_button);
+
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                continueButtonPress();
+            }
+        });
 
         initSpinner();
+    }
+
+    private void continueButtonPress() {
+        if (spinnerSelectedIndex == spinnerSize) {
+            Toast.makeText(this, getString(R.string.please_select_type_string), Toast.LENGTH_SHORT).show();
+        } else {
+            Intent activity = new Intent(this, DataCollectionActivity.class);
+            startActivity(activity);
+            finish();
+        }
     }
 
     private void initSpinner() {
