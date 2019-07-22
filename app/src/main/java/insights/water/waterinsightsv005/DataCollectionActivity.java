de0a1b133116c162426151e63a6a313bd5ffaddc
GarandTyson <gttyson@stanford.edu>
2019-07-22 00:41:32 -0700

    Added dim affect to popup

--- app/src/main/java/insights/water/waterinsightsv005/DataCollectionActivity.java
+++ app/src/main/java/insights/water/waterinsightsv005/DataCollectionActivity.java
@@ -1,30 +1,70 @@
 package insights.water.waterinsightsv005;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.CountDownTimer;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroupOverlay;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
 public class DataCollectionActivity extends AppCompatActivity {
 
+    private static final int STEP_1_TIME = 30;
+
     TextView timerText;
-    public int counter;
+    AppCompatButton startTimerButton;
+    FrameLayout dim;
+
+    public int counter = 0;
+    private boolean canBeClicked = true;
+    private int currStep = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_collection);
 
+        startTimerButton = findViewById(R.id.start_timer_button);
+        dim = findViewById(R.id.background_dim_frame);
         timerText = findViewById(R.id.timer_view);
+
         timerText.setText("0:30");
+
         triggerStep1Popup();
+
+        startTimerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTimerClicked();
+            }
+        });
+    }
+
+    private void startTimerClicked() {
+        if (!canBeClicked) {
+            return;
+        }
+        canBeClicked = false;
+        if (currStep == 1) {
+            setTimer(STEP_1_TIME);
+            canBeClicked = false;
+        }
     }
 
     private void setTimer(int seconds) {
@@ -56,14 +96,23 @@ public class DataCollectionActivity extends AppCompatActivity {
         View popupView = inflater.inflate(R.layout.popup_collection_step1, null);
 
         // create the popup window
-        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        float width = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 300, getResources().getDisplayMetrics());
         int height = LinearLayout.LayoutParams.WRAP_CONTENT;
-        boolean focusable = true; // lets taps outside the popup also dismiss it
-        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        final PopupWindow popupWindow = new PopupWindow(popupView, (int) width, height, true);
+
+        popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                dim.setVisibility(View.GONE);
+            }
+        });
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             popupWindow.setElevation(20);
         }
+
+        dim.setVisibility(View.VISIBLE);
 
         /* Launch popup once activity begins running */
         findViewById(android.R.id.content).post(new Runnable() {
